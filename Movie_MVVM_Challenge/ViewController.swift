29152cf7df7a4e545a58a287e3ba5aee056d0a1a
KienFrank <dinhkien200018@gmail.com>
2024-05-17 10:11:31 +0700

    Initial Commit

---
+++ Movie_MVVM_Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie_MVVM_Challenge
+//
+//  Created by Nguyễn Đình Kiên on 17/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
